a42e8c5c031a471f58166a682c5f7e5ed60c82f2
stefanocoretta <stefano.coretta@gmail.com>
2024-03-30 19:04:05 +0000

    edit plot

--- app.R
+++ app.R
@@ -11,13 +11,16 @@ library(shiny)
 library(bslib)
 library(tidyverse)
 library(Polychrome)
+library(farver)
 
 # Prep data ----
 
-becker2010_wide <- read_csv("data/becker2010.csv",
-                            col_types = str_flatten(c(
-                              "cccccncccncnn", rep("ccnnnn", 14), "_nnnnnn"
-                            )))
+suppressWarnings({
+  becker2010_wide <- read_csv("data/becker2010.csv",
+                              col_types = str_flatten(c(
+                                "cccccncccncnn", rep("ccnnnn", 14), "_nnnnnn"
+                              )))
+})
 
 vow_names <- becker2010_wide %>%
   select(V1OS:V14F4) %>%
@@ -36,12 +39,14 @@ becker2010 <- becker2010_wide %>%
     f2_z = (f2 - mean(f2, na.rm = TRUE)) / sd(f2, na.rm = TRUE),
     f3_z = (f3 - mean(f3, na.rm = TRUE)) / sd(f3, na.rm = TRUE)
   ) %>%
-  ungroup()
+  ungroup() |> 
+  drop_na(f1, f2)
 
-f1_range <- range(becker2010$f1, na.rm = TRUE)
-f2_range <- range(becker2010$f2, na.rm = TRUE)
+fill_pal <- as.character(green.armytage.colors(n = 17))
+col_pal <- ifelse(decode_colour(col_pal, to = "hcl")[, "l"] > 50, "black", "white")
 
-col_pal <- as.character(kelly.colors(n = 22))
+f2_lims <- c(2500, 500)
+f1_lims <- c(900, 150)
 
 # UI ----
 ui <- page_sidebar(
@@ -60,7 +65,7 @@ ui <- page_sidebar(
   ),
   
   card(card_header("Vowel plot"),
-       plotOutput("vowelPlot")),
+       plotOutput("vowelPlot", height = "700px")),
   
   card(card_header("Formant means"),
        tableOutput("vowelTable"))
@@ -73,12 +78,27 @@ server <- function(input, output, session) {
   })
   
   filter_data <- reactive({
-    lang_data <- filter(
-      becker2010,
-      Language == input$language,
-      Dialect == input$dialect
-    ) |>
-      select(vowel, f1, f2) |>
+    if (input$dialect != "NA") {
+      lang_data <- filter(
+        becker2010,
+        Language == input$language,
+        Dialect == input$dialect
+      )
+    } else {
+      lang_data <- filter(
+        becker2010,
+        Language == input$language
+      )
+    }
+    
+    lang_data <- lang_data |>
+      group_by(vowel) |>
+      summarise(
+        f1 = mean(f1, na.rm = TRUE),
+        f2 = mean(f2, na.rm = TRUE),
+        f3 = mean(f3, na.rm = TRUE),
+        f4 = mean(f4, na.rm = TRUE)
+      ) |> 
       arrange(vowel)
 
     return(lang_data)
@@ -87,13 +107,22 @@ server <- function(input, output, session) {
   # Vowel Plot
   output$vowelPlot <- renderPlot({
     lang_data <- filter_data()
+    
+    if (max(lang_data$f1) > f1_lims[1]) {
+      f1_lims[1] <- max(lang_data$f1) + 10
+    }
+    if (max(lang_data$f2) > f2_lims[1]) {
+      f2_lims[1] <- max(lang_data$f2) + 10
+    }
+    
     lang_data |>
-      ggplot(aes(f2, f1, label = vowel, fill = vowel)) +
-      geom_label() +
-      scale_x_reverse(limits = c(2500, 500), position = "top") +
-      scale_y_reverse(limits = c(900, 150), position = "right") +
-      scale_fill_manual(values = col_pal) +
-      theme_dark() +
+      ggplot(aes(f2, f1, label = vowel, fill = vowel, colour = vowel)) +
+      geom_label(size = 6) +
+      scale_x_reverse(limits = f2_lims, position = "top") +
+      scale_y_reverse(limits = f1_lims, position = "right") +
+      scale_fill_manual(values = alpha(fill_pal, 0.7)) +
+      scale_colour_manual(values = col_pal) +
+      theme_light() +
       labs(x = "F2 (Hz)", y = "F1 (Hz)") +
       theme(legend.position = "none") +
       coord_fixed(ratio = 2.5)
